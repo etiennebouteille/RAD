b79a496589614b63028ab75199f4c64b21dfe319
etienne <etienne.bouteille@gmail.com>
2020-01-18 22:32:28 +0100

    temp sensor working and radiatorpower changes accordingly

--- readfile.py
+++ readfile.py
@@ -1,3 +1,7 @@
+import Adafruit_DHT
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
 import pandas as pd
 import time
 
@@ -14,6 +18,11 @@ radiatorOn = False
 
 def getTemperature():
     #TODO: use temperature sensor
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to retrieve data from humidity sensor")
     homeinfo.at[0, 'temperature'] = temperature #replace temperature in the first line of file with measured temp
     return temperature 
 
@@ -31,11 +40,12 @@ def changeTemp(temp):
         return temp
 
 while True:
-    getTemperature()
+    temperature = getTemperature()
+    print("temperature = " + str(temperature))
     radiatorOn = setRadiatorPower()
-
-    if time.time() - oldtime > 3 :
-        temperature = changeTemp(temperature)
-        oldtime = time.time()
-        print(homeinfo)
+    print(radiatorOn)
+    #if time.time() - oldtime > 3 :
+    #    temperature = changeTemp(temperature)
+    #    oldtime = time.time()
+    #    print(homeinfo)
 
